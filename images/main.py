6101171c2e82e1face414176566e628d7a784abc
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-25 21:25:49 -0500

    Update main.py

--- images/main.py
+++ images/main.py
@@ -1,16 +1,45 @@
-import cv2
-import numpy as np
 import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
 
-image = st.camera_input("Show QR code")
+#title of the web-app
+st.title('QR Code Decoding with OpenCV')
 
-if image is not None:
-    bytes_data = image.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+@st.cache
+def qr_code_dec(image):
+    
+    decoder = cv2.QRCodeDetector()
+    data, vertices, rectified_qr_code = decoder.detectAndDecode(image)
+    
+    if len(data) > 0:
+        print("Decoded Data: '{}'".format(data))
 
-    detector = cv2.QRCodeDetector()
+    # Show the detection in the image:
+        show_qr_detection(image, vertices)
+        rectified_image = np.uint8(rectified_qr_code)
+        decoded_data = 'Decoded data: '+ data
+        rectified_image = cv2.putText(rectified_image,decoded_data,(50,350),fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale = 2,
+            color = (250,225,100),thickness =  3, lineType=cv2.LINE_AA)
+           
+    return decoded_data
 
-    data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
 
-    st.write("Here!")
-    st.write(data)
+st.markdown("**Warning** Only add QR-code Images, other images will give out an error")
+
+#uploading the images
+img_file_buffer = st.file_uploader("Upload an image which you want to Decode", type=[ "jpg", "jpeg",'png'])
+
+if img_file_buffer is not None:
+    image = np.array(Image.open(img_file_buffer))
+    st.subheader('Orginal Image')
+
+    #display the image
+    st.image(
+        image, caption=f"Original Image", use_column_width=True
+    ) 
+
+    st.subheader('Decoded data')
+    
+    decoded_data = qr_code_dec(image)
+    st.markdown(decoded_data)
