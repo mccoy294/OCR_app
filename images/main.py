55772ff715a1eda9e03b13471edde64e1c4e3f61
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-29 00:19:43 -0500

    Update main.py

--- images/main.py
+++ images/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import cv2
 import functions
+from PIL import Image
 
 # Title of the web-app
 st.title('Barcode Decoding with OpenCV')
@@ -9,7 +10,7 @@ st.title('Barcode Decoding with OpenCV')
 # Uploading the images
 img_file_buffer = st.file_uploader("Upload an image for decoding", type=["jpg", "jpeg", "png"])
 
-if img_file_buffer is not None:
-    img_file_buffer_str = img_file_buffer.read().decode("utf-8")
-    display = cv2.open(img_file_buffer_str)
-    st.image(display)
+if file is not None:
+
+  image = Image.open(file) # read image with PIL library
+  st.image(image) #display
