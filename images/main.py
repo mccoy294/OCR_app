17f593837f1bd1abefd718d7b139b7b5d3df2b12
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-28 23:52:00 -0500

    Update main.py

--- images/main.py
+++ images/main.py
@@ -9,34 +9,49 @@ st.title('QR Code Decoding with OpenCV')
 
 #uploading the images
 img_file_buffer = st.file_uploader("Upload an image which you want to Decode", type=[ "jpg", "jpeg",'png'])
+    
+
+def preprocess_image(image):
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Apply thresholding to convert to binary image
+    _, binary = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY_INV)
+    
+    return binary
+
+def find_start_and_end(binary_image):
+    # Find the leftmost and rightmost non-zero pixel columns
+    start_col = np.argmax(binary_image.sum(axis=0) > 0)
+    end_col = binary_image.shape[1] - np.argmax(np.flip(binary_image.sum(axis=0)) > 0)
+    
+    return start_col, end_col
+
+def decode_barcode(binary_image, start_col, end_col):
+    # Decoding logic (simplified for example)
+    barcode_data = ""
+    
+    # Assuming each bar is 3 pixels wide
+    bar_width = 3
+    for col in range(start_col, end_col, bar_width):
+        bar_sum = binary_image[:, col:col+bar_width].sum()
+        if bar_sum > binary_image.shape[0] * bar_width // 2:
+            barcode_data += "1"
+        else:
+            barcode_data += "0"
+    
+    return barcode_data
+
+def main():
+    image_path = str(img_file_buffer)
+    image = cv2.imread(image_path)
+    
+    binary_image = preprocess_image(image)
+    start_col, end_col = find_start_and_end(binary_image)
+    
+    barcode_data = decode_barcode(binary_image, start_col, end_col)
+    print("Decoded Barcode Data:", barcode_data)
+
 
 if img_file_buffer is not None:
-    # Convert the uploaded image to a numpy array
-    image = np.array(Image.open(img_file_buffer))
-
-    st.subheader('Orginal Image')
-
-    # Display the original image
-    st.image(image, caption=f"Original Image", use_column_width=True)
-
-    # Convert the image to RGB (EasyOCR uses RGB format)
-    img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Initialize EasyOCR Reader
-    reader = easyocr.Reader(['en'])  # You can specify the languages you want to detect
-
-    # Perform Text Detection
-    results = reader.readtext(img_rgb)
-
-    # Process OCR Results
-    qr_codes = []
-    for (bbox, text, prob) in results:
-        if "http" in text:
-            qr_codes.append(text)
-
-    if qr_codes:
-        st.subheader("Detected QR Codes:")
-        for qr_code in qr_codes:
-            st.write(qr_code)
-    else:
-        st.write("No QR codes detected.")
+    main()
