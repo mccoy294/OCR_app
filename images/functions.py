692247d6c8e954d94dd3f5d28d8eab265960328b
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-29 00:14:13 -0500

    Update functions.py

--- images/functions.py
+++ images/functions.py
@@ -87,3 +87,54 @@ def summarize_dates(date_list):
     DATES_summary = summarize_dates(date_list)"""
     dates_summary = ". ".join(date_list)
     return dates_summary
+
+
+
+
+#--------------------------------------------------------------- QR code ---------------------------------------------------
+def preprocess_image(image):
+    # Convert the image to grayscale
+    if img_file_buffer is not None:
+        image = np.array(Image.open(img_file_buffer))
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Apply thresholding to convert to binary image
+    _, binary = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY_INV)
+    
+    return binary
+
+def find_start_and_end(binary_image):
+    # Find the leftmost and rightmost non-zero pixel columns
+    start_col = np.argmax(binary_image.sum(axis=0) > 0)
+    end_col = binary_image.shape[1] - np.argmax(np.flip(binary_image.sum(axis=0)) > 0)
+    
+    return start_col, end_col
+
+def decode_barcode(binary_image, start_col, end_col):
+    # Decoding logic (simplified for example)
+    barcode_data = ""
+    
+    # Assuming each bar is 3 pixels wide
+    bar_width = 3
+    for col in range(start_col, end_col, bar_width):
+        bar_sum = binary_image[:, col:col+bar_width].sum()
+        if bar_sum > binary_image.shape[0] * bar_width // 2:
+            barcode_data += "1"
+        else:
+            barcode_data += "0"
+    
+    return barcode_data
+
+def main():
+    if img_file_buffer is not None:
+        # Convert BytesIO to a numpy array
+        img_array = np.array(img_file_buffer)
+        
+        # Read the image using OpenCV
+        image = img_array
+        
+        binary_image = preprocess_image(image)
+        start_col, end_col = find_start_and_end(binary_image)
+        
+        barcode_data = decode_barcode(binary_image, start_col, end_col)
+        st.write("Decoded Barcode Data:", barcode_data)
