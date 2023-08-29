c9df013ceb0359b68e4df3e166620174d1e2d575
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-29 00:17:37 -0500

    Update main.py

--- images/main.py
+++ images/main.py
@@ -10,5 +10,6 @@ st.title('Barcode Decoding with OpenCV')
 img_file_buffer = st.file_uploader("Upload an image for decoding", type=["jpg", "jpeg", "png"])
 
 if img_file_buffer is not None:
-    display = cv2.open(str(img_file_buffer))
+    img_file_buffer_str = img_file_buffer.read().decode("utf-8")
+    display = cv2.open(img_file_buffer_str)
     st.image(display)
