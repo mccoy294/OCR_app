1541e869427953c8a69b66c2bec1447be437f010
Ryan McCoy <47424912+mccoy294@users.noreply.github.com>
2023-08-28 23:43:37 -0500

    Update main.py

--- images/main.py
+++ images/main.py
@@ -11,13 +11,32 @@ st.title('QR Code Decoding with OpenCV')
 img_file_buffer = st.file_uploader("Upload an image which you want to Decode", type=[ "jpg", "jpeg",'png'])
 
 if img_file_buffer is not None:
-    # Load the image
-    image = cv2.imread(str(img_file_buffer))
+    # Convert the uploaded image to a numpy array
+    image = np.array(Image.open(img_file_buffer))
+
+    st.subheader('Orginal Image')
+
+    # Display the original image
+    st.image(image, caption=f"Original Image", use_column_width=True)
 
     # Convert the image to RGB (EasyOCR uses RGB format)
     img_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    image = np.array(Image.open(img_file_buffer))
-    st.subheader('Orginal Image')
+    # Initialize EasyOCR Reader
+    reader = easyocr.Reader(['en'])  # You can specify the languages you want to detect
 
+    # Perform Text Detection
+    results = reader.readtext(img_rgb)
 
+    # Process OCR Results
+    qr_codes = []
+    for (bbox, text, prob) in results:
+        if "http" in text:
+            qr_codes.append(text)
+
+    if qr_codes:
+        st.subheader("Detected QR Codes:")
+        for qr_code in qr_codes:
+            st.write(qr_code)
+    else:
+        st.write("No QR codes detected.")
